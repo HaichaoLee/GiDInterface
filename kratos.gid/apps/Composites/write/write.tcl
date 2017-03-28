9d7238c76a6695bba87db7a5898234e856d6b385
Javi Garate <fjgarate@cimne.upc.edu>
2017-03-28 16:16:30 +0200

    #34 Use the new main py script

--- kratos.gid/apps/Composites/write/write.tcl
+++ kratos.gid/apps/Composites/write/write.tcl
@@ -8,8 +8,8 @@ proc Composites::write::Init { } {
 
 
 proc Composites::write::writeCustomFilesEvent { } {
-    write::CopyFileIntoModel "python/dam_main.py"
-    write::RenameFileInModel "dam_main.py" "MainKratos.py"
+    write::CopyFileIntoModel "python/composites_main.py"
+    write::RenameFileInModel "composites_main.py" "MainKratos.py"
    
 }
 
