388f22725202d6e7e5bed67bba386135e9d855c1
Javi Garate <fjgarate@cimne.upc.edu>
2017-04-07 16:43:42 +0200

    #65 Improved app selector window

--- kratos.gid/apps/VirtualWindTunnel/xml/AppSelectorWindowController.tcl
+++ kratos.gid/apps/VirtualWindTunnel/xml/AppSelectorWindowController.tcl
@@ -1,21 +1,41 @@
-
 proc VirtualWindTunnel::AppSelectorWindow { } {
-    variable winpath
     #Init
-    set winpath ".gid.wVWTAppSelector"
-    set w $winpath
+    set w ".gid.wVWTAppSelector"
+    if {[winfo exist $w]} {destroy $w}
+    toplevel $w
+    wm withdraw $w
     
-    if {[winfo exists $w]} {destroy $w}
     
-    toplevel $w -class Toplevel -relief groove 
-    #wm maxsize $w 500 300
-    wm minsize $w 500 250
-    wm overrideredirect $w 0
-    wm resizable $w 1 1
-    wm deiconify $w
-    wm title $w [= "Analysis type"]
-    wm attribute $w -topmost 1
+    set x [expr [winfo rootx .gid]+[winfo width .gid]/2-[winfo width $w]/2]
+    set y [expr [winfo rooty .gid]+[winfo height .gid]/2-[winfo height $w]/2]
     
-    # EmbeddedFluid::xml::BoundingBox::RefreshWindow 
-}
+    wm geom $w +$x+$y
+    wm transient $w .gid  
 
+    InitWindow $w [_ "Virtual Wind Tunnel"] Kratos "" "" 1
+    set initwind $w
+    ttk::frame $w.top
+    ttk::label $w.top.title_text -text [_ "Select analysis type"]
+    ttk::frame $w.information -relief ridge 
+    
+    set appsid [list "Fluid" "EmbeddedFluid"]
+    set appspn [list "Body fitted" "Embedded mesh"]
+    set images [list "fluid.png" "embedded.png"]
+    
+    set col 0
+    set row 0
+    foreach appname $appspn appid $appsid imgid $images {
+        set img  [apps::getImgFrom VirtualWindTunnel $imgid]
+        ttk::button $w.information.img$appid -image $img -command [list apps::setActiveApp $appid]
+        ttk::label $w.information.text$appid -text $appname
+        
+        grid $w.information.img$appid -column $col -row 0
+        grid $w.information.text$appid -column $col -row 1
+        incr col
+    }
+    
+    grid $w.top
+    grid $w.top.title_text
+    
+    grid $w.information
+}
