7d72ed5ed1e2aaffea0abcef28b2cd9a0d58ec4a
Javi Garate <fjgarate@cimne.upc.edu>
2017-04-25 17:48:51 +0200

    #65 Add conditions to window

--- kratos.gid/apps/Fluid/wizard/WindTunnel.tcl
+++ kratos.gid/apps/Fluid/wizard/WindTunnel.tcl
@@ -18,7 +18,7 @@ proc WindTunnelWizard::Wizard::Fluid { win } {
     variable borderwidth
     set gid_height [winfo screenheight .gid]
     set gid_width [winfo screenwidth .gid]
-    Wizard::SetWindowSize [expr int($gid_width *0.5)] [expr int($gid_height *0.5)]
+    Wizard::SetWindowSize [expr int($gid_width *0.5)] [expr int($gid_height *0.7)]
     # Left frame
     set fr1 [ttk::frame $win.fr1 -borderwidth $borderwidth]
 
@@ -92,6 +92,7 @@ proc WindTunnelWizard::Wizard::GetFluidProperties { } {
 proc WindTunnelWizard::Wizard::Conditions { win } {
     variable entrywidth
     variable borderwidth
+    GiD_Process 'Rotate Angle -150 30
     # Left frame
     set fr1 [ttk::frame $win.fr1 -borderwidth $borderwidth]
 
@@ -102,18 +103,59 @@ proc WindTunnelWizard::Wizard::Conditions { win } {
     set labIm [ttk::label $fr2.lIm -image $img1]
     
     # Fluid frame
-    set labfr1 [ttk::labelframe $fr1.lfr1 -text [= "Boundary conditions"] -padding 10 ]
+    set labfr1 [ttk::labelframe $fr1.lfr1 -text [= "Boundary conditions"] -padding 4 ]
     
+    set values [list Front Back Top Bottom Left Rigth]
     # labelframe para el inlet
+        # De las caras libres, inlet value
+        set labinl [ttk::labelframe $labfr1.inlet -text [= "Inlet"] -padding 10 ]
+        set inllbl [ttk::label $labinl.inllbl -text "Inlet face:"]
+        if {![info exists ::Wizard::wprops(Conditions,inlet,value)] || $::Wizard::wprops(Conditions,inlet,value) eq ""} {
+            set ::Wizard::wprops(Conditions,inlet,value) [lindex $values 0]
+        }
+        set comboinlet [ttk::combobox $labinl.cbinlet -values $values -textvariable ::Wizard::wprops(Conditions,inlet,value) -width $entrywidth -state readonly]
+        bind $comboinlet <<ComboboxSelected>> [list WindTunnelWizard::Wizard::ChangeCondition %W] 
+        set ::Wizard::wprops(Conditions,inlet,combo) $comboinlet
         # Tabla para intervalos con init, end, check(val/func), val/func
+
     # labelframe para el outlet
         # De las caras libres, pressure value
+        set labout [ttk::labelframe $labfr1.outlet -text [= "Outlet"] -padding 10 ]
+        set outlbl [ttk::label $labout.outlbl -text "Outlet face:"]
+        if {![info exists ::Wizard::wprops(Conditions,outlet,value)] || $::Wizard::wprops(Conditions,outlet,value) eq ""} {
+            set ::Wizard::wprops(Conditions,outlet,value) [lindex $values 1]
+        }
+        set combooutlet [ttk::combobox $labout.cboutlet -values $values -textvariable ::Wizard::wprops(Conditions,outlet,value) -width $entrywidth -state readonly]
+        bind $combooutlet <<ComboboxSelected>> [list WindTunnelWizard::Wizard::ChangeCondition %W] 
+        set ::Wizard::wprops(Conditions,outlet,combo) $combooutlet
+
     # labelframe para el slip
         # De las caras libres
+        set labslip [ttk::labelframe $labfr1.slip -text [= "Slip"] -padding 10 ]
+        set sliplbl [ttk::label $labslip.sliplbl -text "Slip faces:"]
+        if {![info exists ::Wizard::wprops(Conditions,slip,values)] || $::Wizard::wprops(Conditions,slip,value) eq ""} {
+            set ::Wizard::wprops(Conditions,slip,value) [list [lindex $values 2] [lindex $values 3]]
+        }
+        set checkslipframe [ttk::frame $labslip.checkslipframe]
+        foreach value $values {
+            set labelslip$value [ttk::label $checkslipframe.label$value -text $value]
+            set checkslip$value [ttk::checkbutton $checkslipframe.check$value]
+        }
     # labelframe para el noslit
         # De las caras libres
+        set labnoslip [ttk::labelframe $labfr1.noslip -text [= "No slip"] -padding 10 ]
+        set nosliplbl [ttk::label $labnoslip.sliplbl -text "No slip faces:"]
+        if {![info exists ::Wizard::wprops(Conditions,noslip,values)] || $::Wizard::wprops(Conditions,noslip,value) eq ""} {
+            set ::Wizard::wprops(Conditions,noslip,value) [list [lindex $values 2] [lindex $values 3]]
+        }
+        set checknoslipframe [ttk::frame $labnoslip.checkslipframe]
+        foreach value $values {
+            set labelnoslip$value [ttk::label $checknoslipframe.label$value -text $value]
+            set checknoslip$value [ttk::checkbutton $checknoslipframe.check$value]
+        }
     # labelframe para el immersedbody solo fluid
         #slip/noslip
+    # boton draw
 
     grid $fr1 -column 1 -row 0 -sticky nw
     grid $fr2 -column 2 -row 0 -sticky ne
@@ -122,8 +164,52 @@ proc WindTunnelWizard::Wizard::Conditions { win } {
      grid $labIm -column 0 -row 0 -sticky ne -rowspan 3
     
      # Label frames
-     grid $labfr1 -column 1 -row 0 -sticky wen -ipadx 2  -columnspan 2
-
+     grid $labfr1 -column 1 -row 0 -sticky we -ipadx 2
+        # Inlet
+        grid $labinl -column 1 -row 0 -sticky we -ipadx 2
+        grid $inllbl -column 1 -row 0 -sticky we -ipadx 2
+        grid $comboinlet -column 2 -row 0 -sticky we -ipadx 2
+        # Outlet
+        grid $labout -column 1 -row 1 -sticky we -ipadx 2
+        grid $outlbl -column 1 -row 0 -sticky we -ipadx 2
+        grid $combooutlet -column 2 -row 0 -sticky we -ipadx 2
+        # Slip
+        grid $labslip -column 1 -row 2 -sticky we -ipadx 2
+        grid $sliplbl -column 1 -row 0 -sticky we -ipadx 2
+        grid $checkslipframe -column 1 -row 1 -sticky we -ipadx 2 -columnspan 2
+        set c 0
+        foreach {v1 v2} $values {
+            set l1 labelslip$v1
+            set l2 labelslip$v2
+            grid [set $l1] -column $c -row 0 -sticky we
+            grid [set $l2] -column $c -row 1 -sticky we
+            incr c
+            set b1 checkslip$v1
+            set b2 checkslip$v2
+            grid [set $b1] -column $c -row 0 -sticky we
+            grid [set $b2] -column $c -row 1 -sticky we
+            incr c
+        }
+        # Slip
+        grid $labnoslip -column 1 -row 3 -sticky we -ipadx 2
+        grid $nosliplbl -column 1 -row 0 -sticky we -ipadx 2
+        grid $checknoslipframe -column 1 -row 1 -sticky we -ipadx 2 -columnspan 2
+        set c 0
+        foreach {v1 v2} $values {
+            set l1 labelnoslip$v1
+            set l2 labelnoslip$v2
+            grid [set $l1] -column $c -row 0 -sticky we
+            grid [set $l2] -column $c -row 1 -sticky we
+            incr c
+            set b1 checknoslip$v1
+            set b2 checknoslip$v2
+            grid [set $b1] -column $c -row 0 -sticky we
+            grid [set $b2] -column $c -row 1 -sticky we
+            incr c
+        }
+    
+}
+proc WindTunnelWizard::Wizard::ChangeCondition { inl } {
     
 }
 proc WindTunnelWizard::Wizard::NextConditions { } {
